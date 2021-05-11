2568a873f3bcf9307150620f1174596511415abb
tkkawa <tkm.kawa@gmail.com>
2021-05-11 23:23:12 +0900

    add 441. Arranging Coins

---
+++ 441_arranging-coins.py
@@ -0,0 +1,25 @@
+# n = n
+# time = O(logn)
+# space = O(1)
+# done time = 10m
+
+
+class Solution:
+
+    def arrangeCoins(self, n: int) -> int:
+        low = 1
+        high = n
+        res = 0
+        while low <= high:
+            mid = (low + high) >> 1
+            arithmetic_progression_sum_to_mid = mid*(mid + 1) >> 1
+
+            if arithmetic_progression_sum_to_mid  == n:
+                return mid
+
+            if arithmetic_progression_sum_to_mid  < n:
+                res = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return res
