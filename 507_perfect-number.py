40df5e01f83407a8b6368938bcd9c555450c12d4
tkkawa <tkm.kawa@gmail.com>
2021-05-16 10:36:38 +0900

    add 507. Perfect Number

---
+++ 507_perfect-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+        
+        divisors = [1]
+        div = 2
+        while div ** 2 < num:
+            if num % div == 0:
+                divisors.append(div)
+                if div != num // div:
+                    divisors.append(num // div)
+            div += 1
+
+        return num == sum(divisors)
