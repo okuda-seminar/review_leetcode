09cab2330d69a4a3285102e2e5719ac7fb00bac6
tkkawa <tkm.kawa@gmail.com>
2021-05-29 23:21:42 +0900

    add 1008. Construct Binary Search Tree from Preorder Traversal

---
+++ 1008_construct-binary-search-tree-from-preorder-traversal.py
@@ -0,0 +1,27 @@
+# n = preorder.length
+# time = O(n)
+# space = O(n)
+# done time = 10m
+
+
+class Solution:
+
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+
+        def insert(root, val):
+            if not root:
+                return TreeNode(val)
+
+            if val < root.val:
+                root.left = insert(root.left, val)
+            else:
+                root.right = insert(root.right, val)
+            return root
+
+        root = TreeNode(preorder[0])
+        for i in range(1, len(preorder)):
+            root = insert(root, preorder[i])
+
+        return root
