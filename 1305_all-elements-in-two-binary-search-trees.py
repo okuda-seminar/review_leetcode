ddba8e1a584a93d2d3f7284be8b7dc543cc1abd2
tkkawa <tkm.kawa@gmail.com>
2021-05-31 22:42:20 +0900

    add 1305. All Elements in Two Binary Search Trees

---
+++ 1305_all-elements-in-two-binary-search-trees.py
@@ -0,0 +1,29 @@
+# n = node of number of root1 and root2
+# time = O(n)
+# space = O(n)
+# done time = 20m
+
+
+from collections import deque
+
+
+class Solution:
+
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+
+        def getElements(root: TreeNode, elements: List[int]) -> List[int]:
+            stack = deque([root])
+
+            while stack:
+                curr_node = stack.pop()
+                if not curr_node:
+                    continue
+                elements.append(curr_node.val)
+                stack.append(curr_node.left)
+                stack.append(curr_node.right)
+
+            return elements
+
+        elements = getElements(root1, [])
+        elements = getElements(root2, elements)
+        return sorted(elements)
