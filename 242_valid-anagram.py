1f34f292f01cf813e2e677418b7212bf0fd213b7
kobakobashu <kobaya4@fuji.waseda.jp>
2021-06-01 20:43:12 +0900

    use collections.Counter

--- 242_valid-anagram.py
+++ 242_valid-anagram.py
@@ -3,16 +3,26 @@
 #
 # [242] Valid Anagram
 #
+import collections
+
 
 # @lc code=start
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # use sorting
         """
-        time: O(nlogn)   n : len(s)
+        time: O(nlogn)   n : len(max(s, t))
         space: O(len(n))
-        """
         sorted_s = ','.join(sorted(s))
         sorted_t = ','.join(sorted(t))
         return sorted_s == sorted_t
+        """
+
+        # use collections.Counter
+        """
+        time: O(n)   n : len(max(s, t))
+        space: O(n)
+        """
+        return collections.Counter(s) == collections.Counter(t)
 # @lc code=end
 
