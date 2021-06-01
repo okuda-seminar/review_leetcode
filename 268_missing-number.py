299c97ac5165634896e24ec300aaf25386d27eee
kobakobashu <kobaya4@fuji.waseda.jp>
2021-06-01 21:05:52 +0900

    solve this with O(n) time complexity

--- 268_missing-number.py
+++ 268_missing-number.py
@@ -3,19 +3,31 @@
 #
 # [268] Missing Number
 #
+import collections
 
 # @lc code=start
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # use sorting
         """
         time: O(nlogn) n : len(nums)
         space: O(1)
-        """
         nums.sort()
         for i in range(len(nums)):
             if nums[i] != i:
                 return i
 
         return len(nums)
+        """
+
+        """
+        time: O(n) n : len(nums)
+        space: O(n)
+        """
+        set_nums = set(nums)
+        for i in range(len(nums) + 1):
+            if i in set_nums:
+                continue
+            return i
 # @lc code=end
 
