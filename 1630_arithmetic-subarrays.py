d692735ad88b0ab0dafd6d3a716b6610c8d7466a
tkkawa <tkm.kawa@gmail.com>
2021-06-01 21:14:25 +0900

    add 1630. Arithmetic Subarrays

---
+++ 1630_arithmetic-subarrays.py
@@ -0,0 +1,21 @@
+# n = nums.length
+# m = l.length = r.length <= 500
+# time = O(mnlogn)
+# space = O(m)
+
+
+class Solution:
+
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        res = []
+        for i in range(len(l)):
+            subarray = sorted(nums[l[i]:r[i] + 1])
+            diff = subarray[1] - subarray[0]
+            res.append(True)
+
+            for k in range(2, len(subarray)):
+                if subarray[k] - subarray[k - 1] != diff:
+                    res[-1] = False
+                    break
+
+        return res
