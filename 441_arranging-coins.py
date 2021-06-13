fd7276f6c739a15f718be2c2265ebd455baeadb6
tkkawa <tkm.kawa@gmail.com>
2021-06-13 22:05:34 +0900

    remove unnecessary variable

--- 441_arranging-coins.py
+++ 441_arranging-coins.py
@@ -9,7 +9,6 @@ class Solution:
     def arrangeCoins(self, n: int) -> int:
         low = 0
         high = n + 1
-        res = 0
         while 1 < high - low:
             mid = (low + high) >> 1
             arithmetic_progression_sum_to_mid = mid*(mid + 1) >> 1
@@ -18,8 +17,7 @@ class Solution:
                 return mid
 
             if arithmetic_progression_sum_to_mid  < n:
-                res = mid
                 low = mid
             else:
                 high = mid
-        return res
+        return low
