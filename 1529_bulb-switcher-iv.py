8e48fbcb0d7afe99b8ab0b8e56e57eb336ef832a
tkkawa <tkm.kawa@gmail.com>
2021-06-13 21:26:33 +0900

    modify the line 12 to two lines

--- 1529_bulb-switcher-iv.py
+++ 1529_bulb-switcher-iv.py
@@ -9,6 +9,7 @@ class Solution:
     def minFlips(self, target: str) -> int:
         flip_count = int(target[0])
         for i in range(1, len(target)):
-            if target[i-1] != target[i]: flip_count += 1
+            if target[i-1] != target[i]:
+                flip_count += 1
 
         return flip_count
