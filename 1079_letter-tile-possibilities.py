b93e0eb19cf76edb14ea490f73073c0a6c8ae978
tkkawa <tkm.kawa@gmail.com>
2021-06-13 21:50:34 +0900

    modify variable to instance variable

--- 1079_letter-tile-possibilities.py
+++ 1079_letter-tile-possibilities.py
@@ -7,14 +7,14 @@
 class Solution:
 
     def numTilePossibilities(self, tiles: str) -> int:
-        res = set()
+        self.res = set()
 
         def dfs(path: str, tile: str) -> None:
             if path:
-                res.add(path)
+                self.res.add(path)
             for i in range(len(tile)):
                 dfs(path + tile[i], tile[:i] + tile[i+1:])
 
 
         dfs("", tiles)
-        return len(res)
+        return len(self.res)
