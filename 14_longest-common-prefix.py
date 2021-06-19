45f26289683cd1329ba4ae2cdb5778e1d16db16d
tkkawa <tkm.kawa@gmail.com>
2021-06-19 21:43:27 +0900

    modify all code

--- 14_longest-common-prefix.py
+++ 14_longest-common-prefix.py
@@ -7,15 +7,11 @@
 class Solution:
 
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs: 
-            return ""
-
-        str_min = min(strs)
-        str_max = max(strs)
-        common_prefix_length = 0
-        for i in range(min(len(str_min), len(str_max))):
-            if str_min[i] != str_max[i]:
+        prefix = ''
+        for str_triplet in zip(*strs):
+            if len(set(str_triplet)) == 1:
+                prefix += str_triplet[0]
+            else:
                 break
-            common_prefix_length += 1
 
-        return str_min[:common_prefix_length]
+        return prefix
