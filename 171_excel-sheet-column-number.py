c201cbe47cb41f976713293fef1079488d822c33
kobakobashu <kobaya4@fuji.waseda.jp>
2021-06-20 00:07:30 +0900

    avoid magic number: 26

--- 171_excel-sheet-column-number.py
+++ 171_excel-sheet-column-number.py
@@ -18,8 +18,8 @@ class Solution:
             ref[chr(64 + i)] = i
         columnTitle_list = list(columnTitle)
         cur_num = 0
+        num_alp = 26
         for idx, char in enumerate(columnTitle_list):
-            cur_num += ref[char] * (26 ** (len(columnTitle_list) - idx - 1))
+            cur_num += ref[char] * (num_alp ** (len(columnTitle_list) - idx - 1))
         return cur_num
 # @lc code=end
-
