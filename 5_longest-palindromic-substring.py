0637876ae12075f21819cdb2660f5c697a04e555
kobakobashu <kobaya4@fuji.waseda.jp>
2021-07-01 06:33:17 +0900

    use DP

--- 5_longest-palindromic-substring.py
+++ 5_longest-palindromic-substring.py
@@ -10,7 +10,7 @@ class Solution:
         """
         time: O(n^2)   n : len(s)
         space: O(1)
-        """
+
         if len(s) == 1:
             return s
 
@@ -39,5 +39,28 @@ class Solution:
                     max = s[left:right + 1]
 
         return max
-# @lc code=end
+        """
 
+        # use DP
+        """
+        time: O(n^2)   n : len(s)
+        space: O(n^2)
+        """
+        if not s:
+            return None
+
+        check_palindromic_dp = [[False] * len(s) for _ in range(len(s))]
+        for i in range(len(s)):
+            check_palindromic_dp[i][i] = True
+
+        max_len = 1
+        palindromic_str = s[0]
+        for start in range(len(s) - 1, -1, -1):
+            for end in range(start + 1, len(s)):
+                if s[start] == s[end] and (end - start == 1 or check_palindromic_dp[start + 1][end - 1]):
+                    check_palindromic_dp[start][end] = True
+                    if max_len < end - start + 1:
+                        max_len = end - start + 1
+                        palindromic_str = s[start: end + 1]
+        return palindromic_str
+# @lc code=end
