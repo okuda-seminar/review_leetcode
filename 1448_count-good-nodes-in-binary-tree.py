e1ca8000ba83957215586eeafb3b9ce8fd54ef2f
tkkawa <tkm.kawa@gmail.com>
2021-07-03 10:27:55 +0900

    delete meaningless part and modify to check only the current node

--- 1448_count-good-nodes-in-binary-tree.py
+++ 1448_count-good-nodes-in-binary-tree.py
@@ -6,19 +6,29 @@
 
 from collections import deque
 
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
 
-        stack = collections.deque([[root, root.val]])
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def goodNodes(self, root: TreeNode) -> int:
+        stack = deque([[root, root.val]])
         good_nodes_count = 0
 
         while stack:
             cur_node, max_val = stack.pop()
-            if cur_node.val == max_val:
+            if cur_node is None:
+                continue
+            if max_val <= cur_node.val:
                 good_nodes_count += 1
-            if cur_node.left:
-                stack.append([cur_node.left, max(max_val, cur_node.left.val)])
-            if cur_node.right:
-                stack.append([cur_node.right, max(max_val, cur_node.right.val)])
+                max_val = cur_node.val
+            stack.append([cur_node.left, max_val])
+            stack.append([cur_node.right, max_val])
 
         return good_nodes_count
