b35d56a8f72623c5ef1965c16d82d8b991591541
tkkawa <tkm.kawa@gmail.com>
2021-10-09 23:12:26 +0900

    add 590. N-ary Tree Postorder Traversal

--- problems/590_n-ary-tree-postorder-traversal.py
+++ problems/590_n-ary-tree-postorder-traversal.py
@@ -1,23 +1,51 @@
-# V = node num
-# rimw: O(V)
-# space: O(V)
-"""
+'''
+Problem :
+Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+Answer :
+'''
 # Definition for a Node.
 class Node:
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
-"""
+
 
 class Solution:
-    def postorder(self, root: 'Node') -> List[int]:
-        def traverse(root):
+    def postorder(self, root: 'Node') -> List[int]:        
+        '''DFS : O(V) # V : the number of nodes
+
+        self.res = []
+        def node_traversal(root):
             if root is None:
                 return
-            for c in root.children:
-                traverse(c)
-            ret.append(root.val)
             
-        ret = []
-        traverse(root)
-        return ret
+            for children in root.children:
+                node_traversal(children)
+                
+            self.res.append(root.val)
+            return
+        
+        node_traversal(root)
+        return self.res
+        '''
+
+        '''BFS : O(V) # V : the number of nodes
+
+        from collections import deque
+
+        if root is None:
+            return None
+        
+        stack = deque([root])
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            
+            for children in node.children:
+                stack.append(children)
+            
+        return res[::-1]
+        '''
