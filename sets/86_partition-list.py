ef77f820edd538057348c2a6437e80aad35a6263
kobakobashu <kobaya4@fuji.waseda.jp>
2021-10-28 01:36:45 +0900

    add 86. Partition List

---
+++ sets/86_partition-list.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=86 lang=python3
+#
+# [86] Partition List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        """make partition list
+        
+        Args:
+            head(Optional[ListNode]): a linked list 
+            x(int): integer
+        
+        Returns:
+            Optional[ListNode]: partition linked list 
+        """
+        if not head:
+            return None
+
+        before = before_head = ListNode(0)
+        after = after_head = ListNode(0)
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+
+        after.next = None
+        before.next = after_head.next
+
+        return before_head.next
+# @lc code=end
