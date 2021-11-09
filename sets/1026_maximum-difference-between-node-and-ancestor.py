fd88fc04d4a8645398ba4927d874b372dd3a7bbd
kobakobashu <kobaya4@fuji.waseda.jp>
2021-11-09 19:47:48 +0900

    add solutions using bfs and queue, and modify complexity

--- sets/1026_maximum-difference-between-node-and-ancestor.py
+++ sets/1026_maximum-difference-between-node-and-ancestor.py
@@ -3,6 +3,16 @@
 #
 # [1026] Maximum Difference Between Node and Ancestor
 #
+from collections import deque
+
+
+# use dfs
+"""
+V = node nums
+E = edge nums
+time complexity : O(V + E)
+space complexity : O(V)
+"""
 
 # @lc code=start
 # Definition for a binary tree node.
@@ -11,13 +21,6 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-"""
-V = node nums
-time complexity : O(V)
-space complexity : O(1)
-"""
-
-
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
         """compute max difference
@@ -49,5 +52,72 @@ class Solution:
         self.dfs(root.left, max_num, min_num)
         self.dfs(root.right, max_num, min_num)
 
-# @lc code=end
 
+
+# use dfs by queue
+"""
+V = node nums
+E = edge nums
+time complexity : O(V + E)
+space complexity : O(V)
+"""
+
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        """compute max difference
+
+        Args:
+            root(Optional(TreeNode)): treenode
+        
+        Returns:
+            int: max ancestor difference 
+        """
+        stack = deque()
+        print()
+        stack.append([root, float("inf"), float("-inf")])
+        ans = 0
+        while stack:
+            cur, min_num, max_num = stack.pop()
+            if cur:
+                min_num = min(min_num, cur.val)
+                max_num = max(max_num, cur.val)
+                ans = max(ans, max_num - min_num)
+                stack.append([cur.left, min_num, max_num])
+                stack.append([cur.right, min_num, max_num])
+
+        return ans
+
+
+
+# use bfs
+"""
+V = node nums
+E = edge nums
+time complexity : O(V + E)
+space complexity : O(V)
+"""
+
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        """compute max difference
+
+        Args:
+            root(Optional(TreeNode)): treenode
+        
+        Returns:
+            int: max ancestor difference 
+        """
+        queue = deque()
+        queue.append([root, float("inf"), float("-inf")])
+        ans = 0
+        while queue:
+            cur, min_num, max_num = queue.popleft()
+            if cur:
+                min_num = min(min_num, cur.val)
+                max_num = max(max_num, cur.val)
+                ans = max(ans, max_num - min_num)
+                queue.append([cur.left, min_num, max_num])
+                queue.append([cur.right, min_num, max_num])
+
+        return ans
+# @lc code=end
