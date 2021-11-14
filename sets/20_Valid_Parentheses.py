d7ef42dd6c845d5ed73784c30fe9824291ba9768
nayuta-ai <nayuta723@gmail.com>
2021-11-15 02:20:14 +0900

    add 20. Valid Parentheses

---
+++ sets/20_Valid_Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bra_dict = {')':'(', '}':'{', ']':'['}
+        stack = []
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                
+                if not stack.pop() == bra_dict[char]:
+                    return False
+                
+        return not stack
