62c0e85703816848b277abeecd91624ce95cb986
tkkawa <tkm.kawa@gmail.com>
2021-11-17 20:08:06 +0900

    avoid sum operation

--- sets/1137_n-th-tribonacci-number.py
+++ sets/1137_n-th-tribonacci-number.py
@@ -12,6 +12,8 @@ Answer :
 class Solution:
 
     DP : O(n)
+
+    from collections import deque
     def tribonacci(self, n: int) -> int:
         '''Compute nth tibonacci number
         Args:
@@ -20,14 +22,18 @@ class Solution:
             int: nth tibonacci number
         '''
         TRY_NUM = 3
-        tri_arr = [0, 1, 1]
-        if n <= TRY_NUM:
+        tri_arr = deque([0, 1, 1])
+        if n < TRY_NUM:
             return tri_arr[n - 1]
 
-        for i in range(TRY_NUM, n + 1):
-            tri_arr[i % TRY_NUM] = sum(tri_arr)
+        sum_tri_arr = sum(tri_arr)
+        
+        for i in range(tri_num, n + 1):
+            tri_arr.append(sum_tri_arr)
+            sum_tri_arr += sum_tri_arr - tri_arr[0]
+            tri_arr.popleft()
 
-        return tri_arr[n % TRY_NUM]
+        return tri_arr[-1]
 
     memorization using lru_cache : O(n)
 
