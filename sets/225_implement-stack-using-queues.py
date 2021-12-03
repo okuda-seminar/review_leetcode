cbc1d24e4bb66654331b748db8e4621d0dc3c28a
kobakobashu <kobaya4@fuji.waseda.jp>
2021-12-04 08:54:41 +0900

    add another solution

--- sets/225_implement-stack-using-queues.py
+++ sets/225_implement-stack-using-queues.py
@@ -3,15 +3,12 @@
 #
 # [225] Implement Stack using Queues
 #
-"""
-n = len(input)
-time complexity : O(n)
-space complexity : O(n)
-"""
 from collections import deque
 
 
 # @lc code=start
+
+# use stack fanction
 class MyStack:
 
     def __init__(self):
@@ -41,9 +38,7 @@ class MyStack:
         Returns:
             int: the element on the top of the stack
         """
-        ans = self.stack.pop()
-        self.stack.append(ans)
-        return ans
+        return self.stack[-1]
 
     def empty(self) -> bool:
         """return true if the stack is empty, false otherwise
@@ -52,6 +47,51 @@ class MyStack:
             bool: true if the stack is empty, false otherwise
         """
         return len(self.stack) == 0
+
+
+# use queue
+class MyStack:
+
+    def __init__(self):
+        """initialize data structure
+        """
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        """push element x to the top of the stack
+
+        Args:
+            x(int): integer
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """Remove the element on the top of the stack and returns it
+
+        Returns:
+            int: the element on the top of the stack
+        """
+        tmp = self.queue
+        self.queue = deque()
+        for _ in range(len(tmp) - 1):
+            self.queue.append(tmp.popleft())
+        return tmp.popleft()
+
+    def top(self) -> int:
+        """return the element on the top of the stack
+
+        Returns:
+            int: the element on the top of the stack
+        """
+        return self.queue[-1]
+
+    def empty(self) -> bool:
+        """return true if the stack is empty, false otherwise
+
+        Returns:
+            bool: true if the stack is empty, false otherwise
+        """
+        return len(self.queue) == 0
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
 # obj.push(x)
