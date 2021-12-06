f6709e402f054f7ab19af2d8d5562e636d9b5e79
kobakobashu <kobaya4@fuji.waseda.jp>
2021-12-07 01:53:30 +0900

    add another solution

--- sets/232_implement-queue-using-stacks.py
+++ sets/232_implement-queue-using-stacks.py
@@ -5,14 +5,14 @@
 #
 
 # @lc code=start
-"""
-n = len(input)
-time complexity : O(n)
-space complexity : O(n)
-"""
+from collections import deque
 
 
-class MyQueue:
+"""
+time complexity : O(1)
+space complexity : O(1)
+"""
+class MyQueue1:
 
     def __init__(self):
         """Initialize data structure
@@ -51,6 +51,57 @@ class MyQueue:
         """
         return len(self.stack) == 0
 
+
+"""
+time complexity : O(n)
+space complexity : O(1)
+"""
+class MyQueue:
+
+    def __init__(self):
+        """Initialize data structure
+        """
+        self.stack = deque()
+
+    def push(self, x: int) -> None:
+        """Push element x to the top of the stack
+
+        Args:
+            x(int): integer
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """Remove the element on the top of the stack and returns it
+
+        Returns:
+            int: the element on the top of the stack
+        """
+        tmp = deque()
+        for _ in range(len(self.stack) - 1):
+            tmp.append(self.stack.pop())
+        result = self.stack.pop()
+
+        for _ in range(len(tmp)):
+            self.stack.append(tmp.pop())
+
+        return result
+
+    def peek(self) -> int:
+        """Returns the element at the front of the stack
+
+        Returns:
+            int: the element on the front of the stack
+        """
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        """Return true if the stack is empty, false otherwise
+
+        Returns:
+            bool: true if the stack is empty, false otherwise
+        """
+        return not self.stack
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
@@ -58,4 +109,3 @@ class MyQueue:
 # param_3 = obj.peek()
 # param_4 = obj.empty()
 # @lc code=end
-
